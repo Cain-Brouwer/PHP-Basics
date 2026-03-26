8612e38214f7928496c9fe27da5a6fce980816dd
Cain Brouwer <caindavid2008@gmail.com>
2026-03-26 08:38:44 +0100

    UPDATE method gemaakt in alle controllers

--- app/controllers/HorlogeController.php
+++ app/controllers/HorlogeController.php
@@ -66,5 +66,42 @@ class HorlogeController extends BaseController
     
                 $this->index();
             }
+
+            public function update($Id)
+            {
+                $data = [
+                    'title' => 'Horloge aanpassen',
+                    'display' => 'none',
+                    'message' => '',
+                    'result' => $this->HorlogesModel->getHorlogeById($Id)
+                ];
+    
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if (empty($_POST['merk']) ||
+                        empty($_POST['model']) ||
+                        empty($_POST['prijs']) ||
+                        empty($_POST['materiaal']) ||
+                        empty($_POST['gewicht']) ||
+                        empty($_POST['waterdichtheid']) ||
+                        empty($_POST['releasedatum']) ||
+                        empty($_POST['type']) || 
+                        empty($_POST['uniekkenmerk'])){
+    
+                        $data['display'] = 'flex';
+                        $data['message'] = 'Vul alle velden in.';
+    
+                    }
+                    else {
+                        $data['display'] = 'flex';
+                        $data['message'] = 'de gegevens zijn opgeslagen.';
+    
+                        $this->HorlogesModel->update($Id, $_POST);
+    
+                        header('Refresh:3 ; url=' . URLROOT . '/HorlogeController/index');
+                    }
+                }
+    
+                $this->view('horloges/update', $data);
         
     }
+}
--- app/controllers/SneakersController.php
+++ app/controllers/SneakersController.php
@@ -62,6 +62,44 @@ class SneakersController extends BaseController
             }
         }
 
-        $this->view('sneakers/create', $data);
+            $this->view('sneakers/create', $data);
+
     }
-}   
+     
+            
+
+        public function update($Id)
+        {
+            $data = [
+                'title' => 'Sneakers aanpassen',
+                'display' => 'none',
+                'message' => '',
+                'result' => $this->sneakersModel->getSneakersById($Id)
+            ];
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (empty($_POST['merk']) ||
+                    empty($_POST['model']) ||
+                    empty($_POST['prijs']) ||
+                    empty($_POST['materiaal']) ||
+                    empty($_POST['gewicht']) ||
+                    empty($_POST['releasedatum']) ||
+                    empty($_POST['type'])) {
+
+                    $data['display'] = 'flex';
+                    $data['message'] = 'Vul alle velden in.';
+
+                }
+                else {
+                    $data['display'] = 'flex';
+                    $data['message'] = 'de gegevens zijn opgeslagen.';
+
+                    $this->sneakersModel->update($Id, $_POST);
+
+                    header('Refresh:3 ; url=' . URLROOT . '/SneakersController/index');
+                }
+            }
+
+            $this->view('sneakers/update', $data);
+    }
+}
